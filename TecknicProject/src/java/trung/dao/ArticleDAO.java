3902bb6e7686e311c98600bbfc38111caf266ec1
Trung <damdinhductrung@gmail.com>
2017-10-24 08:00:43 +0700

    up lên để merge về

--- TecknicProject/src/java/trung/dao/ArticleDAO.java
+++ TecknicProject/src/java/trung/dao/ArticleDAO.java
@@ -10,6 +10,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import trung.dto.ArticleDTO;
 
 /**
@@ -17,10 +19,11 @@ import trung.dto.ArticleDTO;
  * @author Trung
  */
 public class ArticleDAO {
+
     Connection conn;
     PreparedStatement pre;
     ResultSet rs;
-    
+
     //Đóng các kết nối sau khi thực hiện xong 1 hàm
     private void closeConnection() {
         try {
@@ -33,20 +36,19 @@ public class ArticleDAO {
             if (conn != null) {
                 conn.close();
             }
-            
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
     //Các member xem bài post của chính mình
     //Dựa vào ID của member lấy ra ArrayList<ArticleDTO>
     //Lấy ra gồm ID, Title, CreateTime, Status
     //Tra về array list các dto
     public ArrayList<ArticleDTO> findArticleByUserID(int userID) {
         ArrayList<ArticleDTO> result = new ArrayList<>();
-        
+
         try {
             conn = MyConnection.getConnection();
             String sql = "select Id, Title, CreateTime, StatusId from Article "
@@ -54,14 +56,14 @@ public class ArticleDAO {
             pre = conn.prepareStatement(sql);
             pre.setInt(1, userID);
             rs = pre.executeQuery();
-            
+
             while (rs.next()) {
                 ArticleDTO dto = new ArticleDTO();
                 dto.setId(rs.getInt("Id"));
                 dto.setTitle(rs.getString("Title"));
                 dto.setCreateTime(rs.getTimestamp("CreateTime"));
                 dto.setStatusId(rs.getInt("StatusId"));
-                
+
                 result.add(dto);
             }
         } catch (Exception e) {
@@ -69,30 +71,30 @@ public class ArticleDAO {
         } finally {
             closeConnection();
         }
-        
+
         return result;
     }
-    
+
     //Người dùng xem lại nội dung của Article trước khi chỉnh sửa
     //Lấy ra dựa vào ArticleID
     //Lấy Title, [Content], CoverImage, Taglist
     //Trả về ArticleDTO
     public ArticleDTO viewArticleForUpdate(int articleID) {
         ArticleDTO result = null;
-        
+
         try {
             conn = MyConnection.getConnection();
             String sql = "select Title, [Content], CoverImage from Article where Id = ?";
             pre = conn.prepareStatement(sql);
             pre.setInt(1, articleID);
             rs = pre.executeQuery();
-            
+
             if (rs.next()) {
                 result = new ArticleDTO();
                 result.setTitle(rs.getString("Title"));
                 result.setContent(rs.getString("Content"));
                 result.setCoverImage(rs.getString("CoverImage"));
-                
+
                 //Lấy TagList
                 sql = "select TagId from ArticleTag where ArticleId = ?";
                 pre = conn.prepareStatement(sql);
@@ -109,20 +111,21 @@ public class ArticleDAO {
         } finally {
             closeConnection();
         }
-        
+
         return result;
     }
-    
+
 //    Member muốn sửa các bài post đang ở trạng thái draf, remove, reject
 //    Sửa article dựa vào ArticleID
 //    Lấy ra
-//    Trả về giá trị boolean
-    public boolean updateArticle(ArticleDTO dto) { //sua lai
+//    Trả về giá trị 
+//    Ghi chú: 0(Không sửa), 1(Thêm), 2(Xóa)
+    public boolean updateArticle(ArticleDTO dto, Map<Integer, Integer> tagList) {
         boolean result = false;
-        
+
         try {
             conn = MyConnection.getConnection();
-            
+
             //Update bảng Article
             String sql = "update Article set Title  = ?, [Content] = ?, CoverImage = ? where Id = ?";
             pre = conn.prepareStatement(sql);
@@ -130,31 +133,36 @@ public class ArticleDAO {
             pre.setString(2, dto.getContent());
             pre.setString(3, dto.getCoverImage());
             pre.setInt(4, dto.getId());
-            pre.executeUpdate();
-            
-            //Update bảng ArticleTag
-            //Xóa hết tất cả các ArticleTag r sau đó add vào lại
-            sql = "delete from ArticleTag where ArticleId = ?";
-            pre = conn.prepareStatement(sql);
-            pre.setInt(1, dto.getId());
-            pre.executeUpdate();
-            for (int tagId : dto.getTagList()) {
-                sql = "insert into ArticleTag (ArticleId, TagId) values (?,?)";
-                pre = conn.prepareStatement(sql);
-                pre.setInt(1, dto.getId());
-                pre.setInt(2, tagId);
-                pre.execute();
+            if (pre.executeUpdate() == 1) { //nếu update đúng 1 Article
+
+                //Update bảng ArticleTag
+                for (int i = 0; i < tagList.size(); i++) {
+                    if (tagList.get(i) == 1) { //Nếu giá trị của TagID là 1 ta thêm vào DB
+                        sql = "insert into ArticleTag (ArticleId, TagId) values (?,?)";
+                        pre = conn.prepareStatement(sql);
+                        pre.setInt(1, dto.getId());
+                        pre.setInt(2, i);
+                        pre.execute();
+                    } else if (tagList.get(i) == 2) { //Nếu giá trị của TagID là 2 ta xóa
+                        sql = "delete from ArticleTag where ArticleId  = ? and TagId = ?";
+                        pre = conn.prepareStatement(sql);
+                        pre.setInt(1, dto.getId());
+                        pre.setInt(2, i);
+                        pre.execute();
+                    }
+                }
+                result = true;
+
             }
-            result = true;
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
             closeConnection();
         }
-                
+
         return result;
     }
-    
+
     public void updateNumOfView(int articleID) {
         try {
             conn = MyConnection.getConnection();
