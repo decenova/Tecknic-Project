3cdd978c8173ecc88493d8aa373fc794229bc197
Mai Đức Thắng <maiducthang97@gmail.com>
2017-11-13 21:10:55 +0700

    update upload file

--- TecknicProject/src/java/servlet/FileServlet.java
+++ TecknicProject/src/java/servlet/FileServlet.java
@@ -9,32 +9,31 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.nio.file.Files;
- 
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
-@WebServlet(urlPatterns = {"/files/*","/editarticle.jspfiles/*"})
+
+@WebServlet(urlPatterns = {"/files/*", "/editarticle.jspfiles/*"})
 public class FileServlet extends HttpServlet {
- 
-private static final long serialVersionUID = 1L;
- 
+
+    private static final long serialVersionUID = 1L;
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException
-    {
+            throws ServletException, IOException {
         try {
             String filename = URLDecoder.decode(request.getPathInfo().substring(1), "UTF-8");
-        File file = new File(getServletContext().getRealPath("/") + "/img", filename);
-        response.setHeader("Content-Type", getServletContext().getMimeType(filename));
-        response.setHeader("Content-Length", String.valueOf(file.length()));
-        response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
-        Files.copy(file.toPath(), response.getOutputStream());
+            File file = new File(getServletContext().getRealPath("/") + "files", filename);
+            response.setHeader("Content-Type", getServletContext().getMimeType(filename));
+            response.setHeader("Content-Length", String.valueOf(file.length()));
+            response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
+            Files.copy(file.toPath(), response.getOutputStream());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
+
     }
 }
